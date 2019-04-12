a2e48db0caf26016836c572d0257a6456b32273e
Tam <tamtheguy@gmail.com>
2019-04-12 15:24:40 -0400

    wrote the three instruction functions

--- project.c
+++ project.c
@@ -87,7 +87,15 @@ void ALU(unsigned A, unsigned B, char ALUControl, unsigned *ALUresult, char *Zer
 /* 10 Points */
 int instruction_fetch(unsigned PC, unsigned *Mem, unsigned *instruction)
 {
-
+	if (PC % 4 == 0) // Checks to see if the PC is word aligned, e.g. divisible by 4
+	{
+		*instruction = Mem[(PC >> 2)]; // The instruction file says that you just need to do PC >> 2 whenever you go into an address
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
 }
 
 
@@ -98,13 +106,148 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,
                             unsigned *offset, unsigned *jsec)
 {
 
+	*op = instruction >> 26; // Returns first 6 bits
+	*r1 = instruction >> 21 & 31; // 31 represents 11111 in binary, isolates the last 5 bits after shifting 21
+	*r2 = instruction >> 16 & 31; // Returns the 5 bits after r1
+	*r3 = instruction >> 11 & 31; // Returns the 5 bits after r2
+	*funct = instruction & 63; // Since funct is the last 6 bits need to use & with 63 which is 111111 in binary.
+	*offset = instruction & 65535; //since offset is the last 16 bits use & with 2^16 - 1 to isolate last 16 bits.
+	*jsec = instruction & 67108863; //since jsec is the last 26 bits use & with 2^26 - 1 to isolate last 26 bits of instruction.
 }
 
 /* instruction decode */
 /* 15 Points */
 int instruction_decode(unsigned op,struct_controls *controls)
 {
-
+	//The op decides what the other control signals are. The below functions that use the control signals will refer to these values
+	// R-type instruction
+	if (op == 0) // 0 -> 000000
+	{
+		controls->RegDst = 1;
+		controls->Jump = 0;
+		controls->Branch = 0;
+		controls->MemRead = 0;
+		controls->MemtoReg = 0;
+		controls->ALUOp = 7;
+		controls->MemWrite = 0;
+		controls->ALUSrc = 0;
+		controls->RegWrite = 1;
+		return 0;
+		
+	}
+	// Add immediate
+	if (op == 8) // 8 -> 001000
+	{
+		controls->RegDst = 0;
+		controls->Jump = 0;
+		controls->Branch = 0;
+		controls->MemRead = 0;
+		controls->MemtoReg = 0;
+		controls->ALUOp = 0;
+		controls->MemWrite = 0;
+		controls->ALUSrc = 1;
+		controls->RegWrite = 1;
+		return 0;
+	}
+	// Load word 
+	if (op == 35) // 35 -> 100011
+	{
+		controls->RegDst = 0;
+		controls->Jump = 0;
+		controls->Branch = 0;
+		controls->MemRead = 1;
+		controls->MemtoReg = 1;
+		controls->ALUOp = 0;
+		controls->MemWrite = 0;
+		controls->ALUSrc = 1;
+		controls->RegWrite = 1;
+		return 0;
+	}
+	// Save word
+	if (op == 43) // 43 -> 101011
+	{
+		controls->RegDst = 0;
+		controls->Jump = 0;
+		controls->Branch = 0;
+		controls->MemRead = 0;
+		controls->MemtoReg = 0;
+		controls->ALUOp = 0;
+		controls->MemWrite = 1;
+		controls->ALUSrc = 1;
+		controls->RegWrite = 0;
+		return 0;
+	}
+	// Load upper immediate 
+	if (op == 15) // 15 -> 001111
+	{
+		controls->RegDst = 0;
+		controls->Jump = 0;
+		controls->Branch = 0;
+		controls->MemRead = 0;
+		controls->MemtoReg = 0;
+		controls->ALUOp = 6;
+		controls->MemWrite = 0;
+		controls->ALUSrc = 1;
+		controls->RegWrite = 1;
+		return 0;
+	}	
+	// Branch on equal
+	if (op == 4) // 4 -> 000100
+	{
+		controls->RegDst = 2;
+		controls->Jump = 0;
+		controls->Branch = 1;
+		controls->MemRead = 0;
+		controls->MemtoReg = 0;
+		controls->ALUOp = 1;
+		controls->MemWrite = 0;
+		controls->ALUSrc = 0;
+		controls->RegWrite = 0;
+		return 0;
+	}	
+	// Set less than immediate
+	if (op == 10) // 10 -> 001010
+	{
+		controls->RegDst = 0;
+		controls->Jump = 0;
+		controls->Branch = 0;
+		controls->MemRead = 0;
+		controls->MemtoReg = 0;
+		controls->ALUOp = 2;
+		controls->MemWrite = 0;
+		controls->ALUSrc = 1;
+		controls->RegWrite = 1;
+		return 0;
+	}	
+	// Set less than immediate unsigned
+	if (op == 11) // 11 -> 001011
+	{
+		controls->RegDst = 0;
+		controls->Jump = 0;
+		controls->Branch = 0;
+		controls->MemRead = 0;
+		controls->MemtoReg = 0;
+		controls->ALUOp = 3;
+		controls->MemWrite = 0;
+		controls->ALUSrc = 1;
+		controls->RegWrite = 1;
+		return 0;
+	}	
+	// Jump
+	if (op == 2) // 2 -> 000010
+	{
+		controls->RegDst = 2;
+		controls->Jump = 1;
+		controls->Branch = 0;
+		controls->MemRead = 0;
+		controls->MemtoReg = 0;
+		controls->ALUOp = 0;
+		controls->MemWrite = 0;
+		controls->ALUSrc = 2;
+		controls->RegWrite = 0;
+		return 0;
+	}	
+	return 1;
 }
 
 /* Read Register */
